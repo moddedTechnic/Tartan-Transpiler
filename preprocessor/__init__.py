b22c0f2443ebef10564462fb8d37df0d7030c7e5
Jonathan Leeming <jpleeming51@gmail.com>
2020-11-10 13:15:59 +0000

    Added start for regions

--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -6,6 +6,10 @@ class PreProcessor:
 	def remove_comments(self, code: str) -> str:
 		return '\n'.join([ l.split('##')[0] for l in code.split('\n') ])
 
+	def region(self, code: str) -> str:
+		return code
+
 	def preprocess(self, code: str) -> str:
 		code = self.remove_comments(code)
+		code = self.region(code)
 		return code
