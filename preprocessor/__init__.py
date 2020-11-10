9eeab3332c1b13ed550b91e6ee52b8908e61b566
Jonathan Leeming <jpleeming51@gmail.com>
2020-11-10 13:29:51 +0000

    Multiline comments

--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -4,7 +4,31 @@ class PreProcessor:
 		self.options = options
 
 	def remove_comments(self, code: str) -> str:
-		return '\n'.join([ l.split('##')[0] for l in code.split('\n') ])
+		code = '\n'.join([ l.split('##')[0] for l in code.split('\n') ])
+		
+		running = True
+		while running:
+			symbol = ''
+			start_comment = -1
+			end_comment = -1
+
+			for i, c in enumerate(str(code)):
+				if c in (' ', '\n', '\r', '\t'):
+					if symbol == '#{':
+						start_comment = i - 2
+					elif symbol == '}#':
+						end_comment = i
+						code = code[:start_comment] + code[end_comment:]
+						break
+					symbol = ''
+					continue
+
+				symbol += c
+
+			else:
+				running = False
+
+		return code
 
 	def region(self, code: str) -> str:
 		return code
